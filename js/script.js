e0dfefac0e42ac8487bec0396edd68a8ee317d52
surruhwater <sekprincess@gmail.com>
2020-10-05 20:30:38 -0500

    Made my rocket lift off

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
+
+// function doCoolStuff changes elements with ID 'cool' to have class name 'cool red'
+var doCoolStuff = function () {
+	// This saves the current class name of the element where we are changing the class to 'cool red'
+	var currentClassName = document.getElementById('cool').className;
+	if (currentClassName == 'cool') {
+		document.getElementById('cool').className = 'cool red';
+	} else {
+		document.getElementById('cool').className = 'cool';
+	}
+
+}
+
+var sayMyName = function (name) {
+	console.log("My name is " + name);
+}
+
+
 var car = {
 			make: 'VW',
 			type: 'Polo',
